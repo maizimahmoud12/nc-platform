6f6ecbe966b91a90876ab00cbb6115eb71cc281f
maizimahmoud12 <maizimahmoud12@gmail.com>
2026-04-24 02:20:07 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,7 @@ const path = require('path');
 const fncRoutes = require('./routes/fncRoutes');
 
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
